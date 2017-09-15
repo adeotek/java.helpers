f08eadab9c60e92175ba57452671815ee4d37c45
George Benjamin-Schonberger <george.benjamin@gmail.com>
2017-09-15 16:20:36 +0300

    RELEASE 1.0.7 - AbstractMessageProcessor changes

--- src/main/java/com/hinter/java/rabbitmq/utils/AbstractMessageProcessor.java
+++ src/main/java/com/hinter/java/rabbitmq/utils/AbstractMessageProcessor.java
@@ -5,12 +5,21 @@ import com.google.gson.JsonObject;
 public abstract class AbstractMessageProcessor {
     protected MessageProcessorStructure _processor;
 
-    public AbstractMessageProcessor(MessageProcessorStructure processor) throws Exception {
+    public AbstractMessageProcessor() {}
+
+//    public AbstractMessageProcessor(MessageProcessorStructure processor) throws Exception {
+//        if (processor==null) {
+//            throw new Exception("Invalid MessageProcessorStructure instance");
+//        }
+//        _processor = processor;
+//    }//AbstractMessageProcessor
+
+    public void SetMessageProcessorStructure(MessageProcessorStructure processor) throws Exception {
         if (processor==null) {
             throw new Exception("Invalid MessageProcessorStructure instance");
         }
         _processor = processor;
-    }//AbstractMessageProcessor
+    }//SetMessageProcessorStructure
 
     public abstract boolean processMessage(JsonObject json);
 }//AbstractMessageProcessor
